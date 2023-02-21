38ae2190e98262db16d4549972b8b24905207540
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-02-21 22:30:13 +0100

    added description and styles in about me section

--- src/Components/AboutMe.tsx
+++ src/Components/AboutMe.tsx
@@ -2,9 +2,19 @@ import "./scss/AboutMe.scss"
 function AboutMe() {
   return (
     <div className="about-me-wrapper">
-      <div className="welcome">Welcome</div>
+      <div className="circle-1"></div>
+      <div className="circle-2-no-fill"></div>
+      <div className="welcome">Welcome!</div>
       <div className="my-name">Hi, I am Emiri.</div>
-      {/* <div>test</div> */}
+      <div className="job-title">Frontend Developer</div>
+      <div className="about-me-description">
+        Occasionally design UI and I enjoy having responsibilities for both
+        creating UI that I envision and giving them shape by coding. I am
+        passionate for both personal growth and software development and have
+        the proven ability to work cross-functionally. Besides that, I also have
+        experience working internationally, so I can easily adjust myself to new
+        environments and love new challenges.
+      </div>
     </div>
   )
 }
