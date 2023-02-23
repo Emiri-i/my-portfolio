9a65e3d2f02855545d32a04343c34e69694eb521
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-02-23 22:24:49 +0100

    modified css

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,9 +1,11 @@
 import AboutMe from "./AboutMe"
+import TechSkills from "./TechSkills"
 import "./scss/Main.scss"
 function Main() {
   return (
     <main className="main-wrapper">
       <AboutMe />
+      <TechSkills />
     </main>
   )
 }
---
+++ src/Components/TechSkills.tsx
@@ -0,0 +1,20 @@
+import "./scss/TechSkills.scss"
+
+function TechSkills() {
+  return (
+    <div className="tech-skills-wrapper">
+      <div className="tech-skills-title">Tech Skills / Tools</div>
+      <div className="language-wrapper">
+        <div className="skill language">React</div>
+        <div className="skill language">Redux</div>
+        <div className="skill language">TypeScript</div>
+        <div className="skill language">JavaScript</div>
+        <div className="skill language">Vue</div>
+        <div className="skill language">Vuetify</div>
+        <div className="skill language">HTML</div>
+        <div className="skill language">SCC / SASS</div>
+      </div>
+    </div>
+  )
+}
+export default TechSkills
