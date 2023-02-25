906ba22817a28f2fd4e0ba817da4985502eba7c7
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-02-25 19:21:39 +0100

    created project section

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,11 +1,13 @@
 import AboutMe from "./AboutMe"
 import TechSkills from "./TechSkills"
+import Projects from "./Projects"
 import "./scss/Main.scss"
 function Main() {
   return (
     <main className="main-wrapper">
       <AboutMe />
       <TechSkills />
+      <Projects />
     </main>
   )
 }
---
+++ src/Components/Projects.tsx
@@ -0,0 +1,57 @@
+import newsAppPhoto from "../img/sc_news_app.png"
+import bookSearchAppPhoto from "../img/sc_book_search_app.png"
+import { FaGithub } from "react-icons/fa"
+import "./scss/Projects.scss"
+function Projects() {
+  const goToNewsAppGitHub = () => {
+    window.open("https://github.com/Emiri-i/news-app")
+  }
+  const goToBookSearchAppGitHub = () => {
+    window.open("https://github.com/Emiri-i/book-search-app")
+  }
+  return (
+    <>
+      <div className="projects-section-wrapper">
+        <div className="projects-title">Projects</div>
+        <div className="projects-each-content-wrapper">
+          <img src={newsAppPhoto} alt="news app image" />
+          <div className="project-explanation-wrapper">
+            <div className="project-each-title">NEWS APP</div>
+            <div className="project-explanation">
+              This is an app that you can find the latest news by filtering
+              category, country, and keyword.
+            </div>
+            <div className="skills-used">
+              React, Typescript, Javascript, HTML, CSS, SASS
+            </div>
+            <div className="project-github-link" onClick={goToNewsAppGitHub}>
+              <FaGithub className="skill-icon" size="1.5em" title={"GitHub"} />
+              <div className="project-github-icon">Go to Github Page</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="projects-each-content-wrapper second-project">
+          <div className="project-explanation-wrapper">
+            <div className="project-each-title">BOOK SEARCH APP</div>
+            <div className="project-explanation">
+              This is an app that you can find books, and keep the record of the
+              books you finished reading.
+            </div>
+            <div className="skills-used">
+              Vue, Vuetify, Typescript, Javascript, HTML, CSS, LocalStorage
+            </div>
+            <div
+              className="project-github-link"
+              onClick={goToBookSearchAppGitHub}>
+              <FaGithub className="skill-icon" size="1.5em" title={"GitHub"} />
+              <div className="project-github-icon">Go to Github Page</div>
+            </div>
+          </div>
+          <img src={bookSearchAppPhoto} alt="book search app image" />
+        </div>
+      </div>
+    </>
+  )
+}
+export default Projects
