34f5ee74b48e8e6e04287640e55b7b8fc4c32fd4
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-02-26 15:07:56 +0100

    added triangle icon in front of each language and added style

--- src/Components/Languages.tsx
+++ src/Components/Languages.tsx
@@ -1,4 +1,5 @@
 import "./scss/Languages.scss"
+import { VscTriangleRight } from "react-icons/vsc"
 
 function Languages() {
   return (
@@ -6,9 +7,18 @@ function Languages() {
       <div className="languages-section-wrapper">
         <div className="section-title">Languages</div>
         <div className="langugages-content">
-          <div className="each-language">Japanese (native)</div>
-          <div className="each-language">English (fluent)</div>
-          <div className="each-language">French (intermidiate)</div>
+          <div className="each-language">
+            <VscTriangleRight className="icon-languages-section" />
+            <div className="language-name">Japanese (native)</div>
+          </div>
+          <div className="each-language">
+            <VscTriangleRight className="icon-languages-section" />
+            <div className="language-name">English (fluent)</div>
+          </div>
+          <div className="each-language">
+            <VscTriangleRight className="icon-languages-section" />
+            <div className="language-name">French (intermidiate)</div>
+          </div>
         </div>
       </div>
     </>
